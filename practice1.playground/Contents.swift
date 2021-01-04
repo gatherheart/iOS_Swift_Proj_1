530a05be112595a251cf94b6f1569e5997fc8e71
gatherheart <gatherheart@naver.com>
2021-01-04 11:45:17 +0900

    [ADD] Practice start

---
+++ practice1.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func add(number1 : Int, number2: Int) -> Int {
+    let sum = number1 + number2
+    return sum
+}
+
+func add(number1 : Int, number2: Int, number3: Int) -> Int {
+    let sum = number1 + number2 + number3
+    return sum
+}
+
+func substract(number1: Int, number2: Int) -> Int {
+    return number1 - number2
+}
+func square(number: Int) -> Int {
+    return number * number
+}
+func divide(number: Int, divisor: Int) -> Int {
+    return number / divisor
+}
+
+add(number1: 123, number2: 234)
+substract(number1: 456, number2: 123)
+square(number: 8)
+divide(number: 256, divisor: 8)
+
+
+var basicSwift = 1
+var _basic_swift = 1
+var Iwant🍺2 = 1
+var BASIC_SWIFT = 1
+var basicSwift1 = 1
+var 🐖Book = 1
+var IntSwift = 1
+var Float = 1
+
+print(basicSwift)
